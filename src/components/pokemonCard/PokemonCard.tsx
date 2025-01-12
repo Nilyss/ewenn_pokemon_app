894f007837e650f7a8bc477a18cb71c78f571dbc
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2025-01-12 18:17:18 +0100

    update card styles

--- src/components/pokemonCard/PokemonCard.tsx
+++ src/components/pokemonCard/PokemonCard.tsx
@@ -2,31 +2,109 @@ import "./pokemonCard.scss";
 
 import { IPokemon } from "../../datas/interfaces/pokemonInterface";
 
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
 export default function PokemonCard({
   pokemon,
 }: Readonly<{
   pokemon: IPokemon;
 }>): ReactElement {
+  const [isDetailsOpen, setIsDetailsOpen] = useState<boolean>(true);
+  const [isStatsOpen, setIsStatsOpen] = useState<boolean>(false);
+  const [isCapaOpen, setIsCapaOpen] = useState<boolean>(false);
+
+  const openDetails = (): void => {
+    setIsDetailsOpen(true);
+    setIsStatsOpen(false);
+    setIsCapaOpen(false);
+  };
+
+  const openStats = (): void => {
+    setIsDetailsOpen(false);
+    setIsStatsOpen(true);
+    setIsCapaOpen(false);
+  };
+
+  const openCapa = (): void => {
+    setIsDetailsOpen(false);
+    setIsStatsOpen(false);
+    setIsCapaOpen(true);
+  };
+
+  const CardButtonContainer = (): ReactElement => {
+    return (
+      <div className={"btnContainer"}>
+        <button onClick={openDetails} className={isDetailsOpen ? "active" : ""}>
+          Détails
+        </button>
+        <button onClick={openStats} className={isStatsOpen ? "active" : ""}>
+          Stats
+        </button>
+        <button onClick={openCapa} className={isCapaOpen ? "active" : ""}>
+          Capacités
+        </button>
+      </div>
+    );
+  };
+
+  const DetailsContent = (): ReactElement => {
+    return (
+      <>
+        <figure>
+          <img
+            src={pokemon.artwork}
+            alt={pokemon.name}
+            className={"pokemonArtwork"}
+          />
+        </figure>
+        <div className={"cardContent"}>
+          <p className={"pokemonID"}>#{pokemon.id}</p>
+          <h3>{pokemon.name}</h3>
+          <p className={"types"}>
+            {pokemon.types.length > 1 ? "Types : " : "Type : "}{" "}
+            {pokemon.types.join(", ")}
+          </p>
+          <CardButtonContainer />
+        </div>
+      </>
+    );
+  };
+
+  const StatsContent = (): ReactElement => {
+    return (
+      <>
+        <h3>Stats</h3>
+        <p>HP : {pokemon.stats.hp}</p>
+        <p>ATK : {pokemon.stats.attack}</p>
+        <p>DEF : {pokemon.stats.defense}</p>
+        <p>Sp.Atk : {pokemon.stats.specialAttack}</p>
+        <p>Sp.Def : {pokemon.stats.specialDefense}</p>
+        <p>Speed : {pokemon.stats.speed}</p>
+        <CardButtonContainer />
+      </>
+    );
+  };
+
+  const CapaContent = (): ReactElement => {
+    return (
+      <>
+        <h3>Capacités</h3>
+        <p>Vol : {pokemon.stats.hp}</p>
+        <p>Poison : {pokemon.stats.attack}</p>
+        <p>Sol : {pokemon.stats.defense}</p>
+        <p>Roche : {pokemon.stats.specialAttack}</p>
+        <p>Insecte : {pokemon.stats.specialDefense}</p>
+        <p>Spectre : {pokemon.stats.speed}</p>
+        <CardButtonContainer />
+      </>
+    );
+  };
+
   return (
     <article id={"pokemonCard"} className={`${pokemon.types[0]}Bkgnd`}>
-      <figure>
-        <img src={pokemon.artwork} alt={pokemon.name} />
-      </figure>
-      <div className={"cardContent"}>
-        <p className={"pokemonID"}>#{pokemon.id}</p>
-        <h3>{pokemon.name}</h3>
-        <p className={'types'}>
-          {pokemon.types.length > 1 ? "Types : " : "Type : "}{" "}
-          {pokemon.types.join(", ")}
-        </p>
-        <div className={"btnContainer"}>
-          <button className={"btn"}>Détails</button>
-          <button className={"btn"}>Stats</button>
-          <button className={"btn"}>Capacités</button>
-        </div>
-      </div>
+      {isDetailsOpen && <DetailsContent />}
+      {isStatsOpen && <StatsContent />}
+      {isCapaOpen && <CapaContent />}
     </article>
   );
 }
--- src/datas/services/getPokemon.service.ts
+++ src/datas/services/getPokemon.service.ts
@@ -2,22 +2,60 @@ import { getRequest } from "../APICalls";
 
 import { IPokemon } from "../interfaces/pokemonInterface";
 import { pokemonModel } from "../models/pokemonModel";
-import {convertTypesToFrench} from "../../utils/Utils.ts";
+import { convertTypesToFrench } from "../../utils/Utils.ts";
+
+interface Stat {
+  base_stat: number;
+  stat: {
+    name: string;
+  };
+}
+
+interface Type {
+  type: {
+    name: string;
+  };
+}
+
+interface Ability {
+  ability: {
+    name: string;
+  };
+}
+
+interface Move {
+  move: {
+    name: string;
+  };
+}
+
+interface EvolutionChain {
+  species: {
+    name: string;
+  };
+  evolves_to: EvolutionChain[];
+}
 
 export class PokemonService {
-  fetchPokemonInFrench = async (pokemonId: string) => {
+  fetchPokemonInFrench = async (
+    pokemonId: string,
+  ): Promise<IPokemon | null> => {
     try {
       const speciesRes = await getRequest(`/pokemon-species/${pokemonId}`);
       const pokemonRes = await getRequest(`/pokemon/${pokemonId}`);
 
       const frenchName =
         speciesRes.data.names.find(
-          (name): boolean => name.language.name === "fr",
+          (name: { language: { name: string }; name: string }): boolean =>
+            name.language.name === "fr",
         )?.name || "Nom non trouvé";
 
       const frenchFlavorText =
         speciesRes.data.flavor_text_entries.find(
-          (entry): boolean => entry.language.name === "fr",
+          (entry: {
+            language: { name: string };
+            flavor_text: string;
+          }): boolean => entry.language.name === "fr",
         )?.flavor_text || "Description non trouvée";
 
       const spriteUrl =
@@ -27,25 +65,32 @@ export class PokemonService {
         pokemonRes.data.sprites.other["official-artwork"].front_default ||
         "Artwork non trouvé";
 
-      const types = pokemonRes.data.types.map((type) => type.type.name) || [];
+      const types =
+        pokemonRes.data.types.map((type: Type) => type.type.name) || [];
       const typesFr = convertTypesToFrench(types);
 
-      const stats = pokemonRes.data.stats.reduce((acc, stat) => {
-        acc[stat.stat.name] = stat.base_stat;
-        return acc;
-      }, {});
+      const stats = pokemonRes.data.stats.reduce(
+        (acc: Record<string, number>, stat: Stat) => {
+          acc[stat.stat.name] = stat.base_stat;
+          return acc;
+        },
+        {},
+      );
 
       const abilities =
-        pokemonRes.data.abilities.map((ability) => ability.ability.name) || [];
+        pokemonRes.data.abilities.map(
+          (ability: Ability) => ability.ability.name,
+        ) || [];
 
-      const moves = pokemonRes.data.moves.map((move) => move.move.name) || [];
+      const moves =
+        pokemonRes.data.moves.map((move: Move) => move.move.name) || [];
 
       const evolutionsRes = await getRequest(
         speciesRes.data.evolution_chain.url,
       );
       const evolutionChain: string[] = [];
 
-      const parseEvolutions = (chain) => {
+      const parseEvolutions = (chain: EvolutionChain) => {
         if (!chain) return;
         evolutionChain.push(chain.species.name);
         chain.evolves_to.forEach(parseEvolutions);
@@ -70,7 +115,9 @@ export class PokemonService {
     }
   };
 
-  fetchAllPokemons = async (limit: number) => {
+  fetchAllPokemons = async (
+    limit: number,
+  ): Promise<{ name: string; url: string }[]> => {
     try {
       const res = await getRequest(`/pokemon?limit=${limit}`);
 
@@ -84,10 +131,13 @@ export class PokemonService {
   fetchAllPokemonsInFrench = async (limit: number): Promise<IPokemon[]> => {
     try {
       const pokemons = await this.fetchAllPokemons(limit);
-      return await Promise.all(
+      const pokemonData = await Promise.all(
         pokemons.map(
-          async (pokemon: Array<object>): Promise<IPokemon | null> => {
-            const id: string = pokemon.url.split("/").filter(Boolean).pop();
+          async (pokemon: {
+            name: string;
+            url: string;
+          }): Promise<IPokemon | null> => {
+            const id: string = pokemon.url.split("/").filter(Boolean).pop()!;
             const res: IPokemon | null = await this.fetchPokemonInFrench(id);
             if (res !== null) {
               return pokemonModel(res);
@@ -96,6 +146,10 @@ export class PokemonService {
             }
           },
         ),
+      );
+
+      return pokemonData.filter(
+        (pokemon): pokemon is IPokemon => pokemon !== null,
       );
     } catch (error) {
       console.error(`Error while fetching pokemons : ${error}`);
